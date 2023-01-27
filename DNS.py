9c9efffa4241e3245b69c7ad7ca6d53f9a4aa83b
Rambod Azimi <59399542+rambodazimi@users.noreply.github.com>
2023-01-27 15:26:33 -0500

    Update DNS.py
    Added some comments
    Completed the arguments

--- DNS.py
+++ DNS.py
@@ -1,31 +1,85 @@
-from socket import * 
-import argparse
+# DNS Client Application
+# Authors:
+# Saghar Sahebi
+# Rambod Azimi
+# ECSE 316 - Assignment 1 - Winter 2023
+# Group 39
+
+"""
+This application is implementing a domain name system (DNS) client using sockets in Python
+It sends a query to the server for the given domain name using a UDP socket protocol and interprets the response to the terminal
+The DNS client should send queries for A (IP address), NS (name server), and MX (mail server) records
+The syntax for running the app is the following:
+python3 DnsClient [-t timeout] [-r max-retries] [-p port] [-mx|-ns] @server name
+"""
+
+import socket
+import argparse # to run a Python script with multiple arguments with cmd
 import time
 
-#arguments 
-default_timeOut = 5
-default_maxRetries = 3
-default_port = 53
-
-# parse the cmd input 
+# default arguments 
+default_timeOut = 5 # gives how long to wait before retransmitting an unanswered query
+default_maxRetries = 3 # max number of times to retransmit an uanswered query before giving up 
+default_port = 53 # UPD port number
 
 def __main__ ():
-    parse = argparse.ArgumentParser("This will query a DNS server")
-    #as mentioned in the doc 
-    #time
+    parse = argparse.ArgumentParser("This will query a DNS server", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # time with the default value of 5 sec
     parse.add_argument("-t", "--timeout", type=int, default= default_timeOut, help="The time to wait before transmitting an unanswered query")
-    #max retries
-    parse.add_argument("-r", "--max-retries", type=int, default= default_maxRetries, help="maximum number of times to retransmit an unanswered query")
-    #port 
-    parse.add_argument("-p", "--port", type=int, default= default_port, help="the UDP port number of the DNS server")
-    #domain name
-    parse.add_argument("-n", type=str, help="Domain name of the IP address")
-    #IP address format of a.b.c.d
-    parse.add_argument("-s", type=str, help= "The IPv4 address of the DNS server")
 
-    group = parse.add_mutually_exclusive_group()
-    #mail server 
-    group.add_argument("-mx", action="store_true", default=False, help="mail query type was sent")
-    #name server 
-    group.add_argument("-ns", action="store_true", default=False, help="name query type was sent")
+    # max retries with the default value of 3
+    parse.add_argument("-r", "--maxretries", type=int, default= default_maxRetries, help="Maximum number of times to retransmit an unanswered query")
 
+    # port with the default value of 53
+    parse.add_argument("-p", "--port", type=int, default= default_port, help="The UDP port number of the DNS server")
+
+    # IPv4 address of the DNS server in a.b.c.d format (required)
+    parse.add_argument(dest="ip_address", type=str, help= "The IPv4 address of the DNS server")
+
+    # domain name (required)
+    parse.add_argument(dest="domain_name", type=str, help="Domain name of the IP address")
+
+    group = parse.add_mutually_exclusive_group() # so that the user can't select both types -mx and -ns together
+
+    # mail server
+    group.add_argument("-mx", action="store_true", default=False, help="Mail server query")
+
+    # name server
+    group.add_argument("-ns", action="store_true", default=False, help="Name server query type")
+
+    # put all the parser arguments into a variable
+    arguments = parse.parse_args()
+
+    # storing each argument to its corresponding variable
+    timeout = arguments.timeout
+    max_retries = arguments.maxretries
+    port = arguments.port
+    ip_address = arguments.ip_address
+    domain_name = arguments.domain_name
+    is_mx = arguments.mx 
+    is_ns = arguments.ns
+    is_A = (not (is_mx)) and (not (is_ns))
+
+    # just for testing purposes
+    print(f"timeout = {timeout}")
+    print(f"max retries = {max_retries}")
+    print(f"port = {port}")
+    print(f"ip address = {ip_address}")
+    print(f"domain name = {domain_name}")
+    print(f"mx = {is_mx}")
+    print(f"ns = {is_ns}")
+    print(f"A = {is_A}")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    __main__()
