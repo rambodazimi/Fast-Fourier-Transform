fffc9dc6225ced49431d23db420b0614b8bcff82
Rambod Azimi <59399542+rambodazimi@users.noreply.github.com>
2023-01-28 17:23:50 -0500

    Update DNS.py

--- DNS.py
+++ DNS.py
@@ -22,10 +22,10 @@ import random
 # default arguments 
 default_timeOut = 5 # gives how long to wait before retransmitting an unanswered query
 default_maxRetries = 3 # max number of times to retransmit an uanswered query before giving up 
-default_port = 53 # UDP port number
-Dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+default_port = 53 # UPD port number
 
 def __main__ ():
+
     parse = argparse.ArgumentParser("This will query a DNS server", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     # time with the default value of 5 sec
@@ -71,66 +71,85 @@ def __main__ ():
     elif (is_ns): # name server query
         queryType = "NS"
         queryNumber = 2 # 0x0002 for a type-NS query
-    else: # type A or standard query (IP address)
+    elif (is_A): # type A or standard query (IP address)
         queryType = "A"
         queryNumber = 1 # 0x0001 for a type-A query
-    
-    result = send_requests(max_retries,timeout,domain_name,queryType,ip_address,port)
-    display_output(result,domain_name)
+    else:
+        invalid_type_error()
 
-def packet_builder(domain_name, queryType):
-    #build request packets 
-    #help from https://stackoverflow.com/questions/24814044/having-trouble-building-a-dns-packet-in-python
-    #>H is used we need big Endian for unsigned short and so we need to reverse the bits 
-    # https://docs.python.org/2/library/struct.html#byte-order-size-and-alignment
-    #as suggested randome numbers will be used each time for the ID
-    #ID
+    # summarize the query to be sent
+    summarize(domain_name, ip_address, queryType)
+
+    # build the DNS packet
+    # request_packet = packet_builder(domain_name, queryType, queryNumber)
+
+    # send a request to the client after building the DNS packet
+    send_request(timeout, max_retries, port, queryNumber, domain_name, ip_address)
+
+    display_output()
+
+def packet_builder(domain_name, queryNumber):
+    # build request packets 
+    # help from https://stackoverflow.com/questions/24814044/having-trouble-building-a-dns-packet-in-python
+    # >H is used we need big Endian for unsigned short and so we need to reverse the bits 
+    
+    # generate a 16-bit random number for ID for each request
     req_pkt = struct.pack(">H", random.getrandbits(16))
-    #Flag
-    req_pkt += struct.pack(">H", 0x100)
-    #QDCOUNT
-    req_pkt += struct.pack(">H", 0x0001)
-    #ANCOUNT
-    req_pkt += struct.pack(">H", 0x0000)
-    #NSCOUNT
-    req_pkt += struct.pack(">H", 0x0000)
-    #ARCOUNT
-    req_pkt += struct.pack(">H", 0x0000)
-    
-    #parsing the name server
-    for i in domain_name.split("."):
-        req_pkt += struct.pack(">B", len(i))
-        for part in i:
-            req_pkt+= struct.pack("c", part.encode('utf-8'))
-    #QNAME
-    req_pkt += struct.pack(">B", 0x0000)
-    #QTYPE
-    req_pkt += struct.pack(">H", queryType)
-    #QCLASS
-    req_pkt += struct.pack(">H", 0x0001)
 
+    # Flag
+    req_pkt += struct.pack(">H", 0x0100) # 0x0100 = 256 = 0b 0000 0001 0000 0000
+    # QR is 0 (query), OPCODE (4 bit) is 0, AA and CC are 0, RD is 1 (recursion), Z (3 bit) is 0, RCODE (4 bit) is 0 (no error in request)
+
+    # QDCOUNT
+    req_pkt += struct.pack(">H", 0x0001) # always set to 1
+
+    # ANCOUNT
+    req_pkt += struct.pack(">H", 0x0000) # number of Resource Records in the answer section
+
+    # NSCOUNT
+    req_pkt += struct.pack(">H", 0x0000) # set to 0 (ignore)
+
+    # ARCOUNT
+    req_pkt += struct.pack(">H", 0x0000) # number of Resource Records in the additional records section
+    
+    # parsing the name server
+    # QNAME
+    for i in domain_name.split("."): # domain name is a sequence of labels separated by dots
+        req_pkt += struct.pack(">B", len(i)) # put the length of each section into question
+        for part in i:
+            req_pkt+= struct.pack("c", part.encode('utf-8')) # put the characters of each section into question
+    req_pkt += struct.pack(">B", 0) # put 0 at the end of QNAME field
+
+    # QTYPE
+    req_pkt += struct.pack(">H", queryNumber) # specifying the type of query
+
+    # QCLASS
+    req_pkt += struct.pack(">H", 0x0001) # Always 1 representing an Internet address
     return req_pkt
 
-#we will send maximum number of retries to
-def send_requests(max_retries,timeout, domain_name,queryType,ip_address,port):
-    for i in range(max_retries):
-        Dns_socket.settimeout(timeout)
+def send_request(timeout, max_retries, port, query_number, domain_name, ip_address):
+    finished = False
+    for i in range(max_retries): # try sending request max_retries number of times
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create a client socket object using IPV4 and UDP protocols
+        client_socket.settimeout(timeout) # set the timeout for the client socket
         try:
-            #stater the timer and then send the request 
             start_time = time.time()
-            #send the request 
-            Dns_socket.sendto(packet_builder(domain_name,queryType),(ip_address,port))
-            response, _ = Dns_socket.recvfrom(512)
-            #end time to be able to find the interval
+            # without connecting to any server (TCP), with UDP we can send the request packet to the tuple (ip address, port number)
+            client_socket.sendto(packet_builder(domain_name, query_number), (ip_address, port))
+            (answer, answer_address) = client_socket.recvfrom(1024) # wait until get an reply from the server
+            finished = True
             end_time = time.time()
-            print(f"Response received after {end_time-start_time} seconds ({i} retries)")
-            break
+            time_taken = end_time - start_time # compute the length of time taken to send the request and receive a response from the server
+            print(f"Response received after {time_taken} seconds ({i} retries)")
+            if (finished): # if a response is received, break from the for loop
+                break
         except socket.timeout:
-            if (i == max_retries):
-                print(f"ERROR   Maximum numbere of retries {max_retries} exceeded")
-            else: 
-                print(f"ERROR   Time Out")
-    return response
+            print(f"ERROR    Time Out Error. You set {timeout} seconds to wait")
+            if(i >= max_retries):
+                print(f"Maximum number of retries {max_retries} exceeded")
+
+    return answer # return the response received from the server
+
 
 #same as build packets but unbuild them to be able to decode them later 
 def unbuild_packet(result):
@@ -148,22 +167,26 @@ def unbuild_packet(result):
     
     list= [id_unpack,flags_unpack,qdCount_unpack,anCount_unpack,anCount_unpack]
     return list
-    
-def display_output(result,domain_name):
+
+
+def display_output():
     anCount = unbuild_packet(result).list[3]
     if (anCount):
         print(f"Answer Section ({anCount} records)")
     else:
         print(f"NOTFOUND")
 
-
+# summarizes the query that is going to be sent
 def summarize(domain_name, ip_address, queryType):
     print(f"DnsClient sending request for: {domain_name}")
     print(f"Server: {ip_address}")
     print(f"Request type: {queryType}")
 
 
-
+def invalid_type_error():
+    print("The query type is invalid!")
+    print("Terminating the DNS client application...")
+    exit()
 
 
 
